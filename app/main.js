dbafc010418971a4711205e39421173934fe965d
Steve Kinney <hello@stevekinney.net>
2017-02-10 12:20:43 -0700

    Create updateMenu function

--- app/main.js
+++ app/main.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const { app, Menu, Tray } = require('electron');
 
+const clippings = [];
 let tray = null;
 
 app.on('ready', () => {
@@ -8,13 +9,25 @@ app.on('ready', () => {
 
   tray = new Tray(path.join(__dirname, '/Icon.png'));
 
+  updateMenu();
+
+  tray.setToolTip('Clipmaster');
+});
+
+const updateMenu = () => {
   const menu = Menu.buildFromTemplate([
+    {
+      label: 'Create New Clipping',
+      click() { null; }
+    },
+    { type: 'separator' },
+    ...clippings.map((clipping, index) => ({ label: clipping })),
+    { type: 'separator' },
     {
       label: 'Quit',
       click() { app.quit(); }
     }
   ]);
 
-  tray.setToolTip('Clipmaster');
   tray.setContextMenu(menu);
-});
+};
