041d29c4c414145a378931d37ed9eaa58cb4c3b2
Steve Kinney <hello@stevekinney.net>
2017-02-10 12:07:01 -0700

    Hide dock icon on macOS

--- app/main.js
+++ app/main.js
@@ -3,6 +3,8 @@ const { app, Menu, Tray } = require('electron');
 let tray = null;
 
 app.on('ready', () => {
+  if (app.dock) app.dock.hide();
+
   tray = new Tray(__dirname + '/Icon.png');
 
   const menu = Menu.buildFromTemplate([
